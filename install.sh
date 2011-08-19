cffa70367c6bd147bc044130f6a4078ef5c5d550
mihasya <m@mihasya.com>
2011-08-18 20:28:59 -0700

    make install script work when symlinks are to non-existent files

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ BACKUP="backups/`date +'%Y%m%d-%H%M%S'`"
 SHDIR=$PWD
 
 for FILE in $SH_FILES; do
-    if [ -e "$HOME/.$FILE" ]; then
+    if [ -e "$HOME/.$FILE" -o -L "$HOME/.$FILE" ]; then
         if [ -L "$HOME/.$FILE" ]; then
             echo "not backing up ~/.$FILE, it is a symlink -> `readlink $HOME/.$FILE`"
             rm "$HOME/.$FILE"
